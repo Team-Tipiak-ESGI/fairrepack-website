e7f1452608245d8eca26efa6494c3d0ebea69b03
Erwan <erwan.legloannec@outlook.com>
2021-04-10 16:42:04 +0200

    Export stocks as XML

---
+++ api/stock/read.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . "/../../utils/database.php";
+
+$warehouse = $_GET['warehouse'] ?? NULL;
+
+$db = getDatabaseConnection();
+$sql = "SELECT r.id_reference as reference, r.brand, r.name, count(r.id_reference) AS count FROM product
+        JOIN reference r ON r.id_reference = product.reference "
+        . (!empty($warehouse) ? " WHERE warehouse = ? " : " ") .
+        " GROUP BY r.id_reference";
+
+$rows = databaseSelectAll($db, $sql, [$warehouse]);
+
+$xml = new SimpleXMLElement('<stocks/>');
+
+foreach ($rows as $row) {
+    $reference = $xml->addChild("reference");
+    $reference->addAttribute("id", $row['reference']);
+    $reference->addAttribute("amount", $row['count']);
+    $reference->addAttribute("brand", $row['brand']);
+    $reference->addAttribute("name", $row['name']);
+}
+
+header('Content-Type: application/xml');
+
+echo $xml->asXML();
