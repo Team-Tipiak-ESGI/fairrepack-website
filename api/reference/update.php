7ed3351f3c5cf5970036f263aa7b8b96205f3f39
Erwan <erwan.legloannec@outlook.com>
2021-04-25 14:07:30 +0200

    Fix

--- api/reference/update.php
+++ api/reference/update.php
@@ -19,38 +19,38 @@ if (isset($_POST["uuid_reference"])) {
     $set = [];
     $params = [];
 
-    if (isset($_POST["brand"])
-    $set[] = "brand";
-    $params[] = "brand";
-}
-if (isset($_POST["name"])
-$set[] = "name";
-$params[] = "name";
-}
-if (isset($_POST["value"])
-$set[] = "value";
-$params[] = "value";
-}
-if (isset($_POST["type"])
-$set[] = "type";
-$params[] = "type";
-}
+    if (isset($_POST["brand"])) {
+        $set[] = "brand";
+        $params[] = "brand";
+    }
+    if (isset($_POST["name"])) {
+        $set[] = "name";
+        $params[] = "name";
+    }
+    if (isset($_POST["value"])) {
+        $set[] = "value";
+        $params[] = "value";
+    }
+    if (isset($_POST["type"])) {
+        $set[] = "type";
+        $params[] = "type";
+    }
 
-$params[] = $uuid_reference;
+    $params[] = $uuid_reference;
 
-$db = getDatabaseConnection();
-$sql = "UPDATE product SET " . join(", ", $set) . " WHERE uuid_reference = ?";
-$success = databaseUpdate($db, $sql, $params);
+    $db = getDatabaseConnection();
+    $sql = "UPDATE product SET " . join(", ", $set) . " WHERE uuid_reference = ?";
+    $success = databaseUpdate($db, $sql, $params);
 
-if ($success) {
-    $reference = getProductbyId($uuid_reference);
+    if ($success) {
+        $reference = getProductbyId($uuid_reference);
 
-    http_response_code(201); // CREATED
-    header("Content-Type: application/json");
-    echo json_encode($reference);
-} else {
-    http_response_code(400);
-}
+        http_response_code(201); // CREATED
+        header("Content-Type: application/json");
+        echo json_encode($reference);
+    } else {
+        http_response_code(400);
+    }
 } else {
     http_response_code(400); // BAD_REQUEST
 }
