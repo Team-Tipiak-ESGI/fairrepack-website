790f7987575c290f09bcd04bda68c6a1c82d2b65
astalios <37106010+Astalios@users.noreply.github.com>
2021-05-20 11:12:25 +0200

    sad
    sad

--- 404.php
+++ 404.php
@@ -2,6 +2,7 @@
 
 <main>
     <section class="container text-center">
+        <img src="/public/assets/sad.png" alt="cTrist">
         <h1>404</h1>
         <p class="lead">The page or resource you are trying to read does not exists.</p>
         <p>Go back to <a href="/">home page</a> or <a href="#" onclick="window.history.back();">previous page</a>.</p>
