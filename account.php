dddc09332a102de4538b1156ad50fe9877030b52
HeartBtz <67628834+HeartBtz@users.noreply.github.com>
2021-06-19 20:39:52 +0200

    reindent

--- account.php
+++ account.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php"; ?>
 
-<main class="container my-5">
+    <main class="container my-5">
         <div class="modal fade" id="modalPP" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
             <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
@@ -10,137 +10,194 @@
                     </div>
                     <div class="modal-body">
                         <h3><span data-i18n>php.account.collected_data</span></h3>
-                        <p><strong><span data-i18n>php.variables.disclaimer</span></strong><span data-i18n>php.account.no_data_used</span></p>
+                        <p><strong><span data-i18n>php.variables.disclaimer</span></strong><span data-i18n>php.account.no_data_used</span>
+                        </p>
                         <ul>
                             <li><span data-i18n>php.account.email_address</span></li>
                             <li><span data-i18n>php.account.login_history</span></li>
                         </ul>
 
                         <h3><span data-i18n>php.account.stored_datas</span></h3>
-                        <p><strong><span data-i18n>php.variables.disclaimer</span></strong><span data-i18n>php.account.no_cookies</span></p>
+                        <p><strong><span data-i18n>php.variables.disclaimer</span></strong><span data-i18n>php.account.no_cookies</span>
+                        </p>
                         <ul>
                             <li><span data-i18n>php.account.jwt_session_token</span></li>
                             <li><span data-i18n>php.account.current_shopping_cart</span></li>
                         </ul>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal"><span data-i18n>php.account.understood</span></button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal"><span data-i18n>php.account.understood</span>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
 
-    <section class="container" id="signedOut">
-        <div class="d-flex justify-content-around">
+        <section class="container" id="signedOut">
+            <div class="d-flex justify-content-around">
 
-            <div class="d-flex flex-column">
-                <p><span data-i18n>php.account.already_account</span></p>
-                <form id="accountForm" class="needs-validation" novalidate onsubmit="return (UserController.login(this), false);">
-                    <div class="mb-3">
-                        <label class="d-block" for="loginEmail">Email</label>
-                        <input id="loginEmail" name="email" type="email" class="form-control" autocomplete="email"
-                               placeholder="example@example.com" required>
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="d-block" for="loginPassword"><span data-i18n>php.variables.password</span></label>
-                        <input id="loginPassword" name="password" type="password" class="form-control" minlength="8"
-                               autocomplete="current-password" placeholder="Password" required>
-                        <div id="validationLoginFeedback" class="invalid-feedback">
-                            <span data-i18n>php.account.invalid_pwd</span>
+                <div class="d-flex flex-column">
+                    <p><span data-i18n>php.account.already_account</span></p>
+                    <form id="accountForm" class="needs-validation" novalidate
+                          onsubmit="return (UserController.login(this), false);">
+                        <div class="mb-3">
+                            <label class="d-block" for="loginEmail">Email</label>
+                            <input id="loginEmail" name="email" type="email" class="form-control" autocomplete="email"
+                                   placeholder="example@example.com" required>
                         </div>
-                    </div>
 
-                    <button type="submit" class="btn btn-primary"><span data-i18n>php.account.login</span></button>
-                </form>
+                        <div class="mb-3">
+                            <label class="d-block" for="loginPassword"><span
+                                        data-i18n>php.variables.password</span></label>
+                            <input id="loginPassword" name="password" type="password" class="form-control" minlength="8"
+                                   autocomplete="current-password" placeholder="Password" required>
+                            <div id="validationLoginFeedback" class="invalid-feedback">
+                                <span data-i18n>php.account.invalid_pwd</span>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary"><span data-i18n>php.account.login</span></button>
+                    </form>
+                </div>
+
+                <div class="d-flex flex-column">
+                    <p><span data-i18n>php.account.new_on_fairrepack</span></p>
+                    <form id="accountForm" class="needs-validation" novalidate
+                          onsubmit="return (UserController.signup(this), false);">
+                        <div class="mb-3">
+                            <label class="d-block" for="signupEmail">Email</label>
+                            <input id="signupEmail" name="email" type="email" class="form-control" autocomplete="email"
+                                   placeholder="example@example.com" required>
+                            <div id="validationLoginFeedback" class="invalid-feedback">
+                                <span data-i18n>php.account.invalid_email</span>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="d-block" for="signupPassword"><span
+                                        data-i18n>php.variables.password</span></label>
+                            <input id="signupPassword" name="password" type="password" class="form-control"
+                                   autocomplete="current-password" placeholder="Password" required minlength="8">
+                            <div id="validationLoginFeedback" class="invalid-feedback">
+                                <span data-i18n>php.account.pwd_too_short</span>
+                            </div>
+                        </div>
+
+                        <p class="fw-lighter">
+                            <span data-i18n>php.account.by_signing_in</span>
+                            <a class="text-decoration-none" style="cursor:pointer;" data-bs-toggle="modal"
+                               data-bs-target="#modalPP"><span data-i18n>php.account.privacy_policy</span></a>.
+                        </p>
+                        <button type="submit" class="btn btn-primary"><span data-i18n>php.variables.sign_up</span>
+                        </button>
+                    </form>
+                </div>
+
             </div>
+        </section>
 
-            <div class="d-flex flex-column">
-                <p><span data-i18n>php.account.new_on_fairrepack</span></p>
-                <form id="accountForm" class="needs-validation" novalidate onsubmit="return (UserController.signup(this), false);">
-                    <div class="mb-3">
-                        <label class="d-block" for="signupEmail">Email</label>
-                        <input id="signupEmail" name="email" type="email" class="form-control" autocomplete="email"
-                               placeholder="example@example.com" required>
-                        <div id="validationLoginFeedback" class="invalid-feedback">
-                            <span data-i18n>php.account.invalid_email</span>
-                        </div>
-                    </div>
+        <section class="d-none" id="signedIn">
 
-                    <div class="mb-3">
-                        <label class="d-block" for="signupPassword"><span data-i18n>php.variables.password</span></label>
-                        <input id="signupPassword" name="password" type="password" class="form-control"
-                               autocomplete="current-password" placeholder="Password" required minlength="8">
-                        <div id="validationLoginFeedback" class="invalid-feedback">
-                            <span data-i18n>php.account.pwd_too_short</span>
-                        </div>
-                    </div>
+            <h1>Modifiez vos informations personnelles</h1>
+            <h4>Laisser vide si pas de changement</h4>
+            <br>
 
-                    <p class="fw-lighter">
-                        <span data-i18n>php.account.by_signing_in</span>
-                        <a class="text-decoration-none" style="cursor:pointer;" data-bs-toggle="modal" data-bs-target="#modalPP"><span data-i18n>php.account.privacy_policy</span></a>.
-                    </p>
-                    <button type="submit" class="btn btn-primary"><span data-i18n>php.variables.sign_up</span></button>
-                </form>
-            </div>
-
-        </div>
-    </section>
-
-    <section class="d-none" id="signedIn">
-
-        <h1>Modifiez vos informations personnelles</h1>
-        <h4>Laisser vide si pas de changement</h4>
-        <br>
-
-        <form id="updateAccountForm" class="needs-validation" novalidate onsubmit="return (UserController.update(this), false)">
+            <form id="updateAccountForm" class="needs-validation" novalidate
+                  onsubmit="return (UserController.update(this), false)">
                 <div class="row">
                     <div class="form-group col-md-6">
                         <label for="inputEmail">Email</label>
-                        <input type="email" class="form-control" id="inputEmail" name="email" placeholder="insérer contenu de la bdd">
+                        <input type="email" class="form-control" id="inputEmail" name="email"
+                               placeholder="insérer contenu de la bdd">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="inputusername">Nom d'utilisateur</label>
-                        <input type="email" class="form-control" id="inputUsername" name="username" placeholder="insérer contenu de la bdd OU Vide si ça l'est">
+                        <input type="email" class="form-control" id="inputUsername" name="username"
+                               placeholder="insérer contenu de la bdd OU Vide si ça l'est">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="inputPassword">Entrer votre ancien mot de passe</label>
-                        <input type="lastpassword" class="form-control" id="lastPassword" name="lastpwd" placeholder="Enter last Password">
+                        <input type="lastpassword" class="form-control" id="lastPassword" name="lastpwd"
+                               placeholder="Enter last Password">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="newPassword">Entrer votre nouveau de passe</label>
-                        <input type="password" class="form-control" id="newPassword" name="newpwd" placeholder="Enter new Password">
+                        <input type="password" class="form-control" id="newPassword" name="newpwd"
+                               placeholder="Enter new Password">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="confirmPassword">Confirmer votre nouveau mot de passe</label>
-                        <input type="password" class="form-control" id="confirmPassword" name="confirmpwd" placeholder="Confirm new Password">
+                        <input type="password" class="form-control" id="confirmPassword" name="confirmpwd"
+                               placeholder="Confirm new Password">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="phone">Numéro de téléphone</label>
+                        <input type="text" class="form-control" id="phone" name="phone"
+                               placeholder="insérer contenu de la bdd">
+                    </div>
+                    <div class="form-group col-md-9"></div>
+                </div>
+                <br>
+                <h5>Adresse</h5>
+                <div class="row">
+                    <div class="form-group col-md-2">
+                        <label for="Country">Pays</label>
+                        <input type="text" class="form-control" id="country" name="country"
+                               placeholder="insérer contenu de la bdd">
+                    </div>
+                    <div class="form-group col-md-1">
+                        <label for="zipcode">Code Postal</label>
+                        <input type="text" class="form-control" id="zipcode" name="zipcode"
+                               placeholder="insérer contenu de la bdd">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="city">Ville</label>
+                        <input type="text" class="form-control" id="city" name="city"
+                               placeholder="insérer contenu de la bdd">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="Address">Adresse </label>
+                        <input type="text" class="form-control" id="Address" name="address"
+                               placeholder="insérer contenu de la bdd">
+                    </div>
+                    <div class="form-group col-md-5">
+                        <label for="owner_name">Nom & Prénom du Propriétaire</label>
+                        <input type="text" class="form-control" id="owner_name" name="owner_name"
+                               placeholder="insérer contenu de la bdd">
                     </div>
                 </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="form-group col-md-12">
+                        <label for="add_infos">Informations complémentaires</label>
+                        <textarea type="text" class="form-control" id="add_infos" name="add_infos"
+                                  placeholder="insérer contenu de la bdd" rows="5"></textarea>
+                    </div>
 
-             <div class="form-group">
-                    <label for="inputAddress">Adresse Complète</label>
-                    <input type="text" class="form-control" id="inputAddress" name="address" placeholder="insérer contenu de la bdd">
                 </div>
+                <br>
+                <br>
+                <button type="submit" class="btn btn-primary">Mettre à jour vos informations</button>
+                <button type="button" class="btn btn-danger" onclick="UserController.remove()"><span data-i18n>php.account.delete_ur_acc</span>
+                </button>
+                <button type="button" class="btn btn-danger" onclick="UserController.logout()"><span data-i18n>php.account.logout</span>
+                </button>
+            </form>
             <br>
             <br>
-            <button type="submit" class="btn btn-primary">Mettre à jour vos informations</button>
-            <button type="button" class="btn btn-danger" onclick="UserController.remove()"><span data-i18n>php.account.delete_ur_acc</span></button>
-            <button type="button" class="btn btn-danger" onclick="UserController.logout()"><span data-i18n>php.account.logout</span></button>
-        </form>
-        <br>
-        <br>
-        <h1><span data-i18n>php.variables.your_products</span></h1>
-        <div id="userProducts" class="d-flex justify-content-center flex-wrap"></div>
-    </section>
-</main>
+            <h1><span data-i18n>php.variables.your_products</span></h1>
+            <div id="userProducts" class="d-flex justify-content-center flex-wrap"></div>
+        </section>
+    </main>
 
-<script>
-    const uuid = getToken()?.payload?.uuid;
-    if (uuid !== undefined) {
-        userVue.buildProductDiv(document.querySelector("div#userProducts"), uuid);
-    }
+    <script>
+        const uuid = getToken()?.payload?.uuid;
+        if (uuid !== undefined) {
+            userVue.buildProductDiv(document.querySelector("div#userProducts"), uuid);
+        }
 
-    userVue.updateAccountPage();
-</script>
+        userVue.updateAccountPage();
+    </script>
 
 <?php include "includes/footer.php"; ?>
--- api/user/update.php
+++ api/user/update.php
@@ -25,52 +25,52 @@ $address = $_POST["address"];
 $error = false;
 $errors = [];
 
-if (!empty($email)){
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $datas_to_send = ["email"=>$email];
+if (!empty($email)) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $datas_to_send = ["email" => $email];
         $set[] = "email = ?";
         $params[] = $email;
-    }else{
+    } else {
         $error = true;
         $errors = ["Format de l'email invalide"];
     }
 }
-if (!empty($username)){
-    if (strlen($username) <2 || strlen($username) >30 ){
-        $datas_to_send = ["username"=>$username];
+if (!empty($username)) {
+    if (strlen($username) < 2 || strlen($username) > 30) {
+        $datas_to_send = ["username" => $username];
         $set[] = "username = ?";
         $params[] = $username;
-    }else{
+    } else {
         $error = true;
         $errors = ["Nom d'utilisateur invalide"];
     }
 }
-if (!empty($lastpwd)){
+if (!empty($lastpwd)) {
     $connection = getDatabaseConnection();
     $hashed_password = hash('sha256', $lastpwd . SECRET);
     $sql = "select uuid_user from `user` where uuid = ? and password = ?";
     $res = databaseFindOne($connection, $sql, [$uuid, $hashed_password]);
-    if (!$res){
-        if( strlen($newpwd)<6
-            || strlen($newpwd)>30
+    if (!$res) {
+        if (strlen($newpwd) < 6
+            || strlen($newpwd) > 30
             || !preg_match("#[A-Z]#", $newpwd)
             || !preg_match("#[a-z]#", $newpwd)
-            || !preg_match("#[0-9]#", $newpwd)){
+            || !preg_match("#[0-9]#", $newpwd)) {
             $error = true;
             $errors[] = "Le mot de passe doit contenir Majuscules, minuscules et chiffres à minimat.";
-        }else{
-            if ( $newpwd == $confirmpwd){
+        } else {
+            if ($newpwd == $confirmpwd) {
                 $new_hashed_password = hash('sha256', $newpwd . SECRET);
-                $datas_to_send = ["password"=>$new_hashed_password];
+                $datas_to_send = ["password" => $new_hashed_password];
                 $set[] = "password = ?";
                 $params[] = $new_hashed_password;
 
-            }else{
+            } else {
                 $error = true;
                 $errors[] = "les deux nouveaux mots de passe ne correspondent pas.";
             }
         }
-    }else{
+    } else {
         $error = true;
         $errors[] = "Le mot de passe d'origine ne correspond pas.";
     }
@@ -78,18 +78,19 @@ if (!empty($lastpwd)){
 
 if (!empty($address)) {
     if (strlen($address) < 20 || strlen($address) > 200) {
-        $datas_to_send = ["address"=>$address];
+        $datas_to_send = ["address" => $address];
         $set[] = "address";
         $params[] = $address;
-    }else{
+    } else {
         $error = true;
         $errors[] = "Entrez un format d'adresse valide";
     }
 }
 
+
 $db = getDatabaseConnection();
 $sql = "UPDATE user SET " . join(", ", $set) . " WHERE uuid_user = ?";
-$success = databaseUpdate($db,$sql,$params);
+$success = databaseUpdate($db, $sql, $params);
 
 if ($success) {
     http_response_code(201); // CREATED
