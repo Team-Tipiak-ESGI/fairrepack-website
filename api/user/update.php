b3529833cfdf22b52cb4bb7d9f75d6de7b32edaf
HeartBtz <67628834+HeartBtz@users.noreply.github.com>
2021-06-19 20:46:28 +0200

    ???

--- api/user/update.php
+++ api/user/update.php
@@ -27,13 +27,12 @@ $errors = [];
 
 if (!empty($email)) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $datas_to_send = ["email" => $email];
         $set = [];
         $params = [];
 
         if (!empty($email)) {
             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $datas_to_send = ["email" => $email];
+
                 $set[] = "email = ?";
                 $params[] = $email;
             } else {
@@ -41,69 +40,75 @@ if (!empty($email)) {
                 $errors = ["Format de l'email invalide"];
             }
         }
+    }
+}
+
+if (!empty($username)) {
+    if (strlen($username) < 2 || strlen($username) > 30) {
+        $datas_to_send = ["username" => $username];
         if (!empty($username)) {
-            if (strlen($username) < 2 || strlen($username) > 30) {
-                $datas_to_send = ["username" => $username];
-                if (!empty($username)) {
-                    if (strlen($username) > 2 && strlen($username) < 30) {
-                        $datas_to_send = ["username" => $username];
-                        $set[] = "username = ?";
-                        $params[] = $username;
-                    } else {
-                        $error = true;
-                        $errors = ["Nom d'utilisateur invalide"];
-                    }
-                }
-                if (!empty($lastpwd)) {
-                    $connection = getDatabaseConnection();
-                    $hashed_password = hash('sha256', $lastpwd . SECRET);
-                    $sql = "select uuid_user from `user` where uuid_user = ? and password = ?";
-                    $res = databaseFindOne($connection, $sql, [$uuid, $hashed_password]);
-                    if (!$res) {
-                        if (strlen($newpwd) < 6
-                            || strlen($newpwd) > 30
-                            || !preg_match("#[A-Z]#", $newpwd)
-                            || !preg_match("#[a-z]#", $newpwd)
-                            || !preg_match("#[0-9]#", $newpwd)) {
-                            $error = true;
-                            $errors[] = "Le mot de passe doit contenir Majuscules, minuscules et chiffres à minimat.";
-                        } else {
-                            if ($newpwd == $confirmpwd) {
-                                $new_hashed_password = hash('sha256', $newpwd . SECRET);
-                                $datas_to_send = ["password" => $new_hashed_password];
-                                $set[] = "password = ?";
-                                $params[] = $new_hashed_password;
+            if (strlen($username) > 2 && strlen($username) < 30) {
 
-                            } else {
-                                $error = true;
-                                $errors[] = "les deux nouveaux mots de passe ne correspondent pas.";
-                            }
-                        }
-                    } else {
-                        $error = true;
-                        $errors[] = "Le mot de passe d'origine ne correspond pas.";
-                    }
-                }
+                $set[] = "username = ?";
+                $params[] = $username;
+            } else {
+                $error = true;
+                $errors = ["Nom d'utilisateur invalide"];
+            }
+        }
+    }
+}
+if (!empty($lastpwd)) {
+    $connection = getDatabaseConnection();
+    $hashed_password = hash('sha256', $lastpwd . SECRET);
+    $sql = "select uuid_user from `user` where uuid_user = ? and password = ?";
+    $res = databaseFindOne($connection, $sql, [$uuid, $hashed_password]);
+    if (!$res) {
+        if (strlen($newpwd) < 6
+            || strlen($newpwd) > 30
+            || !preg_match("#[A-Z]#", $newpwd)
+            || !preg_match("#[a-z]#", $newpwd)
+            || !preg_match("#[0-9]#", $newpwd)) {
+            $error = true;
+            $errors[] = "Le mot de passe doit contenir Majuscules, minuscules et chiffres à minimat.";
+        } else {
+            if ($newpwd == $confirmpwd) {
+                $new_hashed_password = hash('sha256', $newpwd . SECRET);
 
-                if (!empty($address)) {
-                    if (strlen($address) < 20 || strlen($address) > 200) {
-                        $datas_to_send = ["address" => $address];
-                        $set[] = "address";
-                        $params[] = $address;
-                    } else {
-                        $error = true;
-                        $errors[] = "Entrez un format d'adresse valide";
-                    }
-                }
+                $set[] = "password = ?";
+                $params[] = $new_hashed_password;
 
-                $params[] = $uuid;
-                $db = getDatabaseConnection();
-                $sql = "UPDATE user SET " . join(", ", $set) . " WHERE uuid_user = ?";
-                $success = databaseUpdate($db, $sql, $params);
+            } else {
+                $error = true;
+                $errors[] = "les deux nouveaux mots de passe ne correspondent pas.";
+            }
+        }
+    } else {
+        $error = true;
+        $errors[] = "Le mot de passe d'origine ne correspond pas.";
+    }
+}
 
-                if ($success) {
-                    http_response_code(201); // CREATED
-                    header("Content-Type: application/json");
-                } else {
-                    http_response_code(400);
-                }
+if (!empty($address)) {
+    if (strlen($address) < 20 || strlen($address) > 200) {
+
+        $set[] = "address";
+        $params[] = $address;
+    } else {
+        $error = true;
+        $errors[] = "Entrez un format d'adresse valide";
+    }
+}
+
+$params[] = $uuid;
+$db = getDatabaseConnection();
+$sql = "UPDATE user SET " . join(", ", $set) . " WHERE uuid_user = ?";
+$success = databaseUpdate($db, $sql, $params);
+
+if ($success) {
+    http_response_code(201); // CREATED
+    header("Content-Type: application/json");
+} else {
+    http_response_code(400);
+}
+
